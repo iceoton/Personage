8def595b2e0a22f17471cee7f64e69cb4d9295b3
banananyo <wazjakorn@gmail.com>
2017-08-09 13:55:27 +0700

    add cancel leave

--- modules/leave/cancel.php
+++ modules/leave/cancel.php
@@ -7,6 +7,6 @@ require("../../core/functions.core.php");
 $getDatabase = new clear_db();
 $connect = $getDatabase->my_sql_connect(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
 $getDatabase->my_sql_set_utf8();
-mysql_query("UPDATE leave_paper SET status=3 WHERE user_key='".addslashes($_GET['mkey'])."' ");
+$getDatabase->my_sql_update("leave_paper","status=3","user_key='".addslashes($_GET['mkey'])."' ");
 $getDatabase->my_sql_close();
 ?> 
